4233cf30094f6087ff87c58b6e41e9e47e6319b8
Jheremy <a22jhepincre@inspedralbes.cat>
2024-09-16 10:17:00 +0200

    project in progress

---
+++ PR0/PR0/front/pages/finish.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <style>
+         body {
+            background-color: #71CBF0;
+        }
+        
+        .center-screen {
+            height: 100vh; 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+    </style>
+</head>
+<body>
+    <div class="center-screen">
+
+    </div>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
--- PR0/PR0/front/pages/juego.php
+++ PR0/PR0/front/pages/juego.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,7 @@
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <style>
         .center-screen {
-            height: 100vh; 
+            height: 100vh;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -19,8 +20,8 @@
         }
 
         .img-question {
-            height: auto; 
-            width: 150px; 
+            height: auto;
+            width: 150px;
         }
 
         .question-text {
@@ -31,43 +32,51 @@
             padding-right: 5px;
         }
 
-        .py-10{
+        .py-10 {
             padding-top: 20px;
             padding-bottom: 20px;
         }
 
         @media (min-width: 768px) {
             .img-question {
-                width: 240px; 
+                width: 240px;
             }
 
             .question-text {
-                font-size: 2rem; 
+                font-size: 2rem;
             }
         }
     </style>
 </head>
+
 <body>
     <?php
-        // Decodificamos el archivo JSON
-        $datas = json_decode(file_get_contents("data.json"), true);
-        
+    // Decodificamos el archivo JSON
+    $datas = json_decode(file_get_contents("data.json"), true);
+
     ?>
 
     <div class="center-screen w-100 text-center">
+        <div class="row w-100">
+            <div class="col-lg-12 col-12" id="containerProgressbar">
+                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar" style="width: 0%"></div>
+                </div>
+            </div>
+        </div>
         <div class="w-100 d-lg-flex justify-content-center align-items-center">
             <!-- Ajustamos la imagen con la clase 'img-question' -->
             <div>
-                <img src="../img/question.png" class="img-question"/>
+                <img src="../img/question.png" class="img-question" />
             </div>
             <p id="question-text" class="question-text"><?= $datas['preguntes'][0]['pregunta'] ?></p>
         </div>
         <div id="answers" class="w-100 row justify-content-center text-center">
-            <?php foreach($datas['preguntes'][0]['respostes'] as $resposta){?>
-            <div class="col-lg-5 col-12 mb-lg-4 mb-2">
-                <button class="btn btn-primary btn-lg w-75 py-10 btnOpcion" data-option-id="<?= $resposta['id']?>"><?= $resposta['resposta']?></button>
-            </div>
-            <?php }?>
+            <?php foreach ($datas['preguntes'][0]['respostes'] as $resposta) { ?>
+                <div class="col-lg-5 col-12 mb-lg-4 mb-2">
+                    <button class="btn btn-primary btn-lg w-75 py-10 btnOpcion" data-option-id="<?= $resposta['id'] ?>"><?= $resposta['resposta'] ?></button>
+                </div>
+            <?php } ?>
         </div>
     </div>
     <script src="../bootstrap/js/bootstrap.min.js"></script>
@@ -77,12 +86,10 @@
         let posicion = 0; // Posición inicial de la pregunta
 
         function cargarPregunta(pos) {
-            // Actualizamos el texto de la pregunta
             document.getElementById('question-text').textContent = preguntas[pos]['pregunta'];
 
-            // Actualizamos las respuestas
             const answersDiv = document.getElementById('answers');
-            answersDiv.innerHTML = ''; // Limpiamos el contenido anterior
+            answersDiv.innerHTML = '';
 
             preguntas[pos]['respostes'].forEach((respuesta) => {
                 let answerBtn = document.createElement('button');
@@ -90,6 +97,8 @@
                 answerBtn.textContent = respuesta['resposta'];
                 answerBtn.setAttribute('data-option-id', respuesta['id']);
                 answerBtn.addEventListener('click', function() {
+                    const correctAnswer = preguntas[posicion].respostes.find((resposta) => resposta.correcta == true);
+                    console.log(correctAnswer)
                     siguientePregunta();
                 });
                 let colDiv = document.createElement('div');
@@ -103,6 +112,12 @@
             // Pasamos a la siguiente pregunta, si hay más
             if (posicion < preguntas.length - 1) {
                 posicion++;
+                let containerProgressbar = document.querySelector('#containerProgressbar');
+                containerProgressbar.innerHTML = `
+                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="${posicion*10}" aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar" style="width: ${posicion*10}%"></div>
+                </div>
+                `;
                 cargarPregunta(posicion);
             } else {
                 alert("Has terminado el cuestionario");
@@ -114,4 +129,5 @@
         });
     </script>
 </body>
+
 </html>
