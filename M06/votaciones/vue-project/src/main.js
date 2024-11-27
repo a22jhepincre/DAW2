85f2901d0dc7b59a8decc0f0f570b0406a9dbfc8
a22jhepincre <a22jhepincre@inspedralbes.cat>
2024-11-27 09:16:21 +0100

    protected by route with auth

--- M06/votaciones/vue-project/src/main.js
+++ M06/votaciones/vue-project/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { Quasar } from 'quasar'
 import { createPinia } from 'pinia'
 
+
 // Import icon libraries
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
--- M06/votaciones/vue-project/src/router/index.js
+++ M06/votaciones/vue-project/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AuthenticatorView from "@/views/AuthenticatorView.vue";
+import { useAuthStore } from '@/stores/authStore';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +10,25 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: true }, // Indica que requiere autenticación
+    },
+    {
+      path: '/auth',
+      name: 'authenticator',
+      component: AuthenticatorView,
     },
   ],
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore(); // Obtén el estado de autenticación
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    // Redirige a /auth si intenta acceder a una ruta protegida
+    next({ name: 'authenticator' });
+  } else {
+    next(); // Permite la navegación
+  }
+});
 
 export default router
